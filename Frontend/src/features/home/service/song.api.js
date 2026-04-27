3c383d61e60584c4bbc1810ef5b293a4f9dbe42e
afzal <write1afzal@gmail.com>
2026-04-27 16:48:34 +0530

    error update

--- Frontend/src/features/home/service/song.api.js
+++ Frontend/src/features/home/service/song.api.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://face-expression-project-1.onrender.com",
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:3000"
+      : "https://face-expression-project-1.onrender.com",
   withCredentials: true
 })
 
 
-export async function getSong({mood}) {
+export async function getSong({ mood }) {
   const response = await api.get("/api/songs?mood=" + mood)
   console.log(response);
   return response.data
